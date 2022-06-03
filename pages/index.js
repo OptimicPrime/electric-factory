c6913b2f53b645feb818b915ba847f3588a32466
OptimicPrime <96891601+OptimicPrime@users.noreply.github.com>
2022-06-03 11:24:20 -0600

    first commit

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home({ posts }) {
   return (
     <div className='container mx-auto px-10 mb-8'>
       <Head>
-        <title>Buzz Learning</title>
+        <title>Electric Factory</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <FeaturedPosts />
